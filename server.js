dd32dbef2cb590d79edf0632b3160fa7ee68b715
LiorGinz-Google <55990516+LiorGinz-Google@users.noreply.github.com>
2020-10-26 14:27:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     console.log('New Connection')
-    res.end('Hello Lior Ginzberg - Now with CI/CD support')
+    res.end('Hello Lior Ginzberg - Now with CI/CD support - Yalla')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('listening'))
